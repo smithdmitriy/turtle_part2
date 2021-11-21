724dad6f981544e7853cca645c31b4528f0a9e2f
smithdmitriy <smithdmitriy@gmail.com>
2021-11-21 23:21:53 +0500

    task 2 -

--- task_2.py
+++ task_2.py
@@ -8,14 +8,22 @@ tr.color('blue', 'black')
 tr.pensize(4)
 tr.speed(1)
 
-def prnt(ax:list, ay:list):
+
+def prnt(ax: list, ay: list, shift:int):
+    size = 80
+    tr.goto((ax[0]+shift) * size, ay[0])
+    tr.pd
     for i in range(len(ax)):
-        tr.goto(ax[i]*40, ay[i]*40)
-    tr.penup()
-    tr.goto(0,0)
+        tr.goto((ax[i]+shift) * size, ay[i] * size)
+    tr.pu()
+    tr.goto((ax[0]+shift)*size, ay[0])
 
-a0x = [0, 1, 1, 0]
-a0y = [2, 2, 0, 0]
+a1x = [0, 1, 1]
+a1y = [1, 2, 0]
+a0x = [0, 0, 1, 1, 0]
+a0y = [0, 2, 2, 0, 0]
 
-prnt(a0x, a0y)
+prnt(a1x, a1y,0)
+prnt(a0x, a0y,2)
+
 
