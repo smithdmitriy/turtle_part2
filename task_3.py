af46fca7fdda54b77a6f32240b0e159af92d2d66
smithdmitriy <smithdmitriy@gmail.com>
2021-11-22 18:58:59 +0500

    task 3 final

--- task_3.py
+++ task_3.py
@@ -1,9 +1,13 @@
 import turtle as tr
 
-tr.shape('turtle')
-tr.color('blue', 'black')
-tr.pensize(4)
-tr.speed(5)
+
+def convert_str_to_turple(s: str) -> tuple:
+    s = s[7:len(s) - 2]
+    s = s.replace(', ', '')
+    tpl = []
+    for i in range(len(s)):
+        tpl.append(int(s[i]))
+    return tuple(tpl)
 
 
 def pr_number(ax: list, ay: list, shift: int):
@@ -16,31 +20,22 @@ def pr_number(ax: list, ay: list, shift: int):
     tr.pu()
     tr.goto((ax[0] + shift) * size, ay[0] * size)
 
+
+tr.shape('turtle')
+tr.color('blue', 'black')
+tr.pensize(4)
+tr.speed(5)
+
 input = open('task_3', 'r')
 ax = []
 ay = []
 for i in range(10):
-
     stx = input.readline()
-    stx = stx[7:len(stx) - 2]
-    stx = stx.replace(', ', '')
-    tplx = []
-    for i in range(len(stx)):
-        tplx.append(int(stx[i]))
-
     sty = input.readline()
-    sty = sty[7:len(sty)- 2]
-    sty = sty.replace(', ', '')
-    tply = []
-    for i in range(len(sty)):
-        tply.append(int(sty[i]))
-
-    ax.append(tuple(tplx))
-    ay.append(tuple(tply))
-
+    ax.append(convert_str_to_turple(stx))
+    ay.append(convert_str_to_turple(sty))
 strx = '01234567890'
 x = int(strx)
-k = 0
 for i in range(len(strx)):
     j = x // 10 ** (len(strx) - i - 1)
     x = x % 10 ** (len(strx) - i - 1)
