317a5e7b7ad42e94c80f7770e2bee37bccfe06a4
smithdmitriy <smithdmitriy@gmail.com>
2021-11-23 17:36:21 +0500

    task 5 -

--- task_5.py
+++ task_5.py
@@ -5,9 +5,8 @@ import turtle as tr
 number_of_turtles = 10
 steps_of_time_number = 100
 
-tr.screensize(canvwidth=200, canvheight=200, bg='red')
+tr.screensize(canvwidth=100, canvheight=100, bg='red')
 pool = [tr.Turtle(shape='circle') for i in range(number_of_turtles)]
-tr.setworldcoordinates(-250, -250, 250, 250)
 for unit in pool:
     unit.turtlesize(0.5)
     unit.pu()
