c829f8110a7b24c93f30045466d2d990bb672f33
smithdmitriy <smithdmitriy@gmail.com>
2021-11-23 17:54:40 +0500

    task 5 add different speed

--- task_5.py
+++ task_5.py
@@ -5,16 +5,17 @@ import turtle as tr
 number_of_turtles = 10
 steps_of_time_number = 100
 
-tr.screensize(canvwidth=100, canvheight=100, bg='red')
 pool = [tr.Turtle(shape='circle') for i in range(number_of_turtles)]
 for unit in pool:
-    unit.turtlesize(0.5)
+    unit.turtlesize(1)
     unit.pu()
+    unit.v = randint(1,12)
     unit.speed(50)
     unit.goto(randint(-200, 200), randint(-200, 200))
     unit.right(randint(0, 360))
 
 
+
 for i in range(steps_of_time_number):
     for unit in pool:
-        unit.forward(1)
+        unit.forward(unit.v)
