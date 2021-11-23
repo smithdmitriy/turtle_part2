2803f77cdafe671c4346f17427ec42d31b12b00f
smithdmitriy <smithdmitriy@gmail.com>
2021-11-23 08:24:47 +0500

    task 4 +

--- task_4.py
+++ task_4.py
@@ -1,6 +1,6 @@
 import turtle as tr
 
-tr.shape('turtle')
+tr.shape('circle')
 tr.color('blue', 'black')
 tr.pensize(2)
 tr.speed(10)
@@ -14,9 +14,9 @@ Vx = 3
 Vy = 8
 dt = 0.5
 ay = 0.2
-ax = 0.005
+ax = 0.003
 
-while Vx > 0.1:
+while Vx > 0:
     tr.goto(x, y)
     x += Vx * dt
     y += Vy * dt - ay * dt ** 2 / 2
@@ -26,4 +26,4 @@ while Vx > 0.1:
     if y < 0:
         Vy = -Vy / 1.4
         y = 0
-        Vx = 0.8 * Vx
+        Vx = 0.9 * Vx
