275918c2e49f1b8b932c82c35d8866a2da38eb08
smithdmitriy <smithdmitriy@gmail.com>
2021-11-28 21:11:24 +0500

    task 5 - -

--- task_5.py
+++ task_5.py
@@ -1,66 +1,56 @@
 from random import *
 import turtle as tr
 
-number_of_turtles = 5
+number_of_turtles = 3
 delta = 50
-steps_of_time_number = 10000
-tr.tracer(2, 60)
-tr.pu()
-tr.speed(0)
-tr.goto(-250, -250)
-tr.pd()
-tr.goto(-250, 250)
-tr.goto(250, 250)
-tr.goto(250, -250)
-arx = [0 for i in range(10)]
-ary = [0 for i in range(10)]
+steps_of_time_number = 10000 * 100
+ball_size = 3
+r_ball = ball_size * 10
+dt = 0.1
+
+tr.tracer(20, 60)
+arx = [0 for i in range(number_of_turtles)]
+ary = [0 for i in range(number_of_turtles)]
+arvx = [0 for i in range(number_of_turtles)]
+arvy = [0 for i in range(number_of_turtles)]
 pool = [tr.Turtle(shape='circle') for i in range(number_of_turtles)]
 j = 0
 for unit in pool:
-
     unit.number = j
-    unit.turtlesize(1.5)
+    unit.turtlesize(ball_size)
     unit.pu()
     unit.vx = random()
-    print(unit.vx)
     unit.ax = 0
     unit.vy = random()
-    print(unit.vy)
     unit.ay = 0
     unit.speed(50)
     unit.x = randint(-200, 200)
-
     unit.y = randint(-200, 200)
     arx[unit.number] = unit.x
     ary[unit.number] = unit.y
+    arvx[unit.number] = unit.vx
+#    arvy[unit.number] = unit.vy
     unit.goto(unit.x, unit.y)
     j += 1
 
 for i in range(steps_of_time_number):
     for unit in pool:
-        for t in range(number_of_turtles):
-            if t != unit.number and abs(unit.x - arx[t]) < delta and abs(unit.y - ary[t]) < delta:
-
-                unit.ax = (1 / (unit.x - arx[t])**2)
-                print(unit.number, unit.ax)
-                unit.ay = (1 / (unit.y - ary[t]))
-                print(unit.number, unit.ay, t)
-
-            if t != unit.number and abs(unit.x - arx[t]) < delta and abs(unit.y - ary[t]) < delta:
-                unit.ax = (1 / (unit.x - arx[t]))
-                print(unit.number, unit.ax)
-                unit.ay = (1 / (unit.y - ary[t]))
-                print(unit.number, unit.ay, t)
-
-        unit.x += unit.vx + unit.ax
-        unit.y += unit.vy + unit.ay
+        unit.x += unit.vx * dt + unit.ax * dt ** 2 / 2
+        unit.y += unit.vy * dt + unit.ay * dt ** 2 / 2
         unit.ax = 0
         unit.ay = 0
         unit.goto(unit.x, unit.y)
         arx[unit.number] = unit.x
         ary[unit.number] = unit.y
+        arvx[unit.number] = unit.vx
+        arvy[unit.number] = unit.vy
 
-        if abs(unit.x) > 250:
+        for t in range(number_of_turtles):
+            if t != unit.number and ((unit.x - arx[t])**2 + (unit.y - ary[t])**2)**0.5 < r_ball * 2:
+                unit.vx = arvx[t]
+                unit.vy = arvy[t]
+
+        if abs(unit.x) > 250 - r_ball:
             unit.vx *= -1
-        if abs(unit.y) > 250:
+        if abs(unit.y) > 250 - r_ball:
             unit.vy *= -1
